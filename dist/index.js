f2d826a06e0feee487624c5626c23c5fd4876f94
Daniel Valadas <info@danielvaladas.com>
2020-05-04 11:24:23 -0400

    reset tsconfig

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,13 +8,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import * as core from '@actions/core';
-import { Octokit } from '@octokit/rest';
-const formatVersion = (tagName) => {
-    const version = { tag: "", major: 0, minor: 0, patch: 0, manifestSafeVersionString: "" };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core = __importStar(require("@actions/core"));
+var rest_1 = require("@octokit/rest");
+var formatVersion = function (tagName) {
+    var version = { tag: "", major: 0, minor: 0, patch: 0, manifestSafeVersionString: "" };
     version.tag = tagName;
-    const regex = /[^0-9.]/;
-    const numbers = tagName.replace(regex, '').split('.');
+    var regex = /[^0-9.]/;
+    var numbers = tagName.replace(regex, '').split('.');
     version.major = parseInt(numbers[0]);
     version.minor = parseInt(numbers[1]);
     version.patch = parseInt(numbers[2]);
@@ -25,26 +61,28 @@ const formatVersion = (tagName) => {
             version.patch.toString().padStart(2, '0');
     return version;
 };
-const run = () => __awaiter(void 0, void 0, void 0, function* () {
-    // Inputs
-    const owner = core.getInput('owner');
-    const repoName = core.getInput('repo');
-    console.log(owner, repoName);
-    // Repository info
-    const octokit = new Octokit({
-        auth: '',
-        userAgent: 'dnn-platform-get-version'
+var run = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var owner, repoName, octokit;
+    return __generator(this, function (_a) {
+        owner = core.getInput('owner');
+        repoName = core.getInput('repo');
+        console.log(owner, repoName);
+        octokit = new rest_1.Octokit({
+            auth: '',
+            userAgent: 'dnn-platform-get-version'
+        });
+        octokit.repos.getLatestRelease({
+            owner: owner,
+            repo: repoName
+        })
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            .then(function (fullfilled) {
+            // Outputs
+            var version = formatVersion(fullfilled.data.tag_name);
+            core.setOutput('version', version);
+        });
+        return [2 /*return*/];
     });
-    octokit.repos.getLatestRelease({
-        owner: owner,
-        repo: repoName
-    })
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        .then((fullfilled) => {
-        // Outputs
-        const version = formatVersion(fullfilled.data.tag_name);
-        core.setOutput('version', version);
-    });
-});
+}); };
 run();
-export default run;
+exports.default = run;
